fa97df2f01d4d8e9f3dde27cf9de065e1e8a02fa
luutien18195 <30953080+luutien18195@users.noreply.github.com>
2020-09-05 22:20:54 +0700

    update git ignore file

---
+++ src/test/java/com/tainika/qlnt/qlnt/QlntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tainika.qlnt.qlnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
